5a9571a9b2ae90bb62aad61554237e77ba077400
ALTRIN JOSHUA A J <ajaltrinjoshua@gmail.com>
2026-07-04 02:02:10 +0530

    Day 2 - Resume upload feature added!

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def home():
 
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    file = request.files["resume"]
+
+    if file:
+
+        file.save(
+            os.path.join(
+                app.config["UPLOAD_FOLDER"],
+                file.filename
+            )
+        )
+
+        return f"""
+          Resume uploaded successfully! <br><br>
+          File Name: {file.filename}
+ """
 
 if __name__ == "__main__":
     app.run(debug=True)

06524e723e4a892cc3cd4f6bf57d843b23fc7026
ALTRIN JOSHUA A J <ajaltrinjoshua@gmail.com>
2026-07-04 23:25:55 +0530

    Day 4 - Added job description matching

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 import os
 from PyPDF2 import PdfReader
+from docx import Document
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
-
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
 
@@ -18,6 +19,8 @@ def home():
 def upload():
 
     file = request.files["resume"]
+
+    occupation = request.form["occupation"]
 
     if file:
 
@@ -28,64 +31,99 @@ def upload():
 
         file.save(filepath)
 
-        reader = PdfReader(filepath)
-
         resume_text = ""
 
-        for page in reader.pages:
-            resume_text += page.extract_text()
+        # PDF extraction
+        if file.filename.endswith(".pdf"):
 
-        # Keep line breaks
-        resume_text = resume_text.replace("\n", "<br>")
+            reader = PdfReader(filepath)
 
-        # Skills to detect
-        skills = [
-            "Python",
-            "Java",
-            "C++",
-            "Machine Learning",
-            "HTML",
-            "CSS",
-            "Flask"
-        ]
+            for page in reader.pages:
 
-        found_skills = []
+                extracted = page.extract_text()
+
+                if extracted:
+                    resume_text += extracted
+
+        # DOCX extraction
+        elif file.filename.endswith(".docx"):
+
+            doc = Document(filepath)
+
+            for paragraph in doc.paragraphs:
+
+                resume_text += paragraph.text + "\n"
+
+        resume_text = resume_text.replace(
+            "\n",
+            "<br>"
+        )
+
+        job_skills = {
+
+            "Full Stack Developer":
+            ["HTML","CSS","JavaScript","Python","Flask","SQL"],
+
+            "Cloud Engineer":
+            ["AWS","Linux","Docker","Kubernetes","Python"],
+
+            "Cyber Security Analyst":
+            ["Networking","Linux","Python","Wireshark"],
+
+            "Data Scientist":
+            ["Python","Machine Learning","Pandas","SQL"]
+        }
+
+        skills = job_skills[occupation]
+
+        found_skills=[]
 
         for skill in skills:
+
             if skill.lower() in resume_text.lower():
-                found_skills.append(skill)
-        
-        # Score ccalculation based on the number of skills found
-        total_skills = len(skills)
-        matched_skills = len(found_skills)
-        score = (matched_skills / total_skills) * 100 if total_skills > 0 else 0
 
-        #List of missing skills
-        missing_skills =[]
+                found_skills.append(skill)
+
+        total_skills=len(skills)
+
+        matched_skills=len(found_skills)
+
+        score=(matched_skills/total_skills)*100
+
+        missing_skills=[]
+
         for skill in skills:
+
             if skill not in found_skills:
+
                 missing_skills.append(skill)
 
-        #Return
-
         return f"""
-        Resume uploaded successfully! <br><br>
+        Resume uploaded successfully!<br><br>
 
-        File Name: {file.filename}<br><br>
+        Occupation:
+        {occupation}<br><br>
+
+        File Name:
+        {file.filename}<br><br>
 
         Resume Content:<br><br>
 
         {resume_text}<br><br>
 
         Detected Skills:<br>
+
         {", ".join(found_skills)}<br><br>
 
         Resume Score:<br>
+
         {score:.0f}/100<br><br>
 
-        Suggestions for Improvement:<br>
-        Add the following skills to your resume to improve your score: {", ".join(missing_skills)}<br><br>
+        Suggestions:<br>
+
+        Add these skills:
+        {", ".join(missing_skills)}
         """
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
